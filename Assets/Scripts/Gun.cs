ac6a58f11618f56088b3146b4834e9c601202dff
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2021-04-07 14:54:23 -0300

    Par de scripts

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public int damage;
+    public float range, fireRate;
+    private float nextTimeToFire = 0;
+
+    public int maxAmmo = 10;
+    public int currentAmmo;
+    public float reloadTime = 1;
+    private bool isReloading;
+    public bool changeWeapon = true;
+
+    public Camera fpsCam;
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+        currentAmmo = maxAmmo;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isReloading)
+            return;
+
+        if (currentAmmo == 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1 / fireRate;
+            Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo)
+        {
+            StartCoroutine(Reload());
+            changeWeapon = false;
+            return;
+        }
+    }
+    void Shoot()
+    {
+        currentAmmo--;
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        _anim.SetInteger("Bullets", currentAmmo);
+        yield return new WaitForSeconds(reloadTime - .25f);
+        yield return new WaitForSeconds(.25f);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+        changeWeapon = true;
+    }
+}
